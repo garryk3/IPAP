ce9afafbfd1c8b5f63637eb5414cf573622acc95
garryk3 <egoruum@gmail.com>
2019-12-06 15:59:55 +0300

    1-1

--- modules/1.js
+++ modules/1.js
@@ -1,4 +1,5 @@
 // найти максимум чисел
+// сложность 5
 const findMaxFromLimitedArgs = (a, b, c) => {
     let max = a;
 
@@ -7,6 +8,7 @@ const findMaxFromLimitedArgs = (a, b, c) => {
 
     return max;
 };
+// сложность 1 + 2 * i
 const findMaximumNumber = (...numbers) => {
     let max = 0;
 
@@ -19,12 +21,14 @@ const findMaximumNumber = (...numbers) => {
 
 const testArgs1 = [10, 50, 25];
 const testArgs2 = [1, 2, 3, 4, 100, 54, 6];
+const testArgs3 = [1, -200, 0, 2, 3, 4, 200, 54, 6];
 
 const max1 = findMaxFromLimitedArgs(...testArgs1);
 const max2 = findMaximumNumber(...testArgs2);
+const max3 = findMaximumNumber(...testArgs3);
 
-if(max1 !== Math.max(...testArgs1) || max2 !== Math.max(...testArgs2)) {
+if(max1 !== Math.max(...testArgs1) || max2 !== Math.max(...testArgs2) || max3 !== Math.max(...testArgs3)) {
     throw new Error('!!!');
 } else {
-    console.log('success', max1, max2);
+    console.log('success', max1, max2, max3);
 }
