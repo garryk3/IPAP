ea4e5088fce081f4ae924300fc77867910b1a1ea
garryk3 <egoruum@gmail.com>
2020-02-14 13:23:53 +0300

    c++ 16

--- homework/c++/16 /16.cpp
+++ homework/c++/16 /16.cpp
@@ -3,7 +3,9 @@
 #include <string>
 #include <cmath>
 #include <fstream>
-
+#include <vector>
+#include <algorithm>
+#include <sstream>
 
 using namespace std;
 
@@ -17,29 +19,79 @@ using namespace std;
 // записать отсортированный массив в файл
 // ------------------------------
 
-const string IN = "in.txt";
-const string OUT = "out.txt";
+const string IN = "/home/projects/my/ipap/homework/c++/16 /in.txt";
+const string OUT = "/home/projects/my/ipap/homework/c++/16/out.txt";
 
-void readFile() {};
+vector<int> readFile(string path = IN) {
+    ifstream infile;
+    infile.open(path);
+    vector<int> numbers;
+    if(infile.is_open()) {
+        int num;
+        while(infile >> num) {
+            numbers.push_back(int(num));
+        }
+        infile.close();
+    }
+            int num;
+        while(infile >> num) {
+            numbers.push_back(int(num));
+        }
+        infile.close();
+    return numbers;
+};
 
-void writeFile() {};
+void writeToStream(ofstream &stream, int val) {
+    stream << val;
+}
+
+void writeToFile(vector<int> &list, string path = OUT) {
+    ofstream outfile;
+    outfile.open(path);
+    if(outfile.is_open()) {
+        // for_each(list.begin(), list.end(), writeToStream);
+        outfile.close();
+    }
+};
+
+void printVector(vector<int> &vec) {
+    vector<int>::const_iterator it;
+    stringstream s;
+
+    for( it = vec.begin(); it != vec.end(); ++it )
+    {
+        if( it != vec.begin() )
+            s << " ";
+
+        s << *it;
+    }
+    cout << "string: " << s.str() << endl;
+};
+
+void readAndWriteSortedList() {
+    vector<int> list = readFile();
+    sort(list.begin(), list.end());
+    printVector(list);
+    // writeToFile(list);
+};
 
 int main() {
-    string filename = "out.txt";
-    ofstream fout;
-    fout.open(filename);
-    if(fout.is_open()) {
-        string line = "ops";
-        while(line != "") {
-            cout << "enter line ";
-            char buffer[1000];
-            cin.getline(buffer, 1000);
-            line = buffer;
-            if(line != "") {
-                fout << line << endl;
-            }
-        }
-        fout.close();
-    }
+    readAndWriteSortedList();
+    // string filename = IN;
+    // ofstream fout;
+    // fout.open(filename);
+    // if(fout.is_open()) {
+    //     string line = "ops";
+    //     while(line != "") {
+    //         cout << "enter line ";
+    //         char buffer[1000];
+    //         cin.getline(buffer, 1000);
+    //         line = buffer;
+    //         if(line != "") {
+    //             fout << line << endl;
+    //         }
+    //     }
+    //     fout.close();
+    // }
     return 0;
 }
